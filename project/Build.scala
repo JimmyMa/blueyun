b7ed2a4bfb54d924e642fe605dd7bdfabc4defae
Yun Feng Ma <yunfengjv@sina.com>
2013-01-15 13:54:44 +0800

    cc

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "com.amazonaws" % "aws-java-sdk" % "1.3.11"
+      "com.amazonaws" % "aws-java-sdk" % "1.3.11",
       "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
