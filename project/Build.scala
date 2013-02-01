ea6738668c6e5abc8c1397e0a09d5d80ce63bc68
Yun Feng Ma <yunfengjv@sina.com>
2013-02-01 14:58:31 +0800

    gzip

--- project/Build.scala
+++ project/Build.scala
@@ -12,8 +12,28 @@ object ApplicationBuild extends Build {
       "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-
-    )
-
+	val gzippableAssets = SettingKey[PathFinder]("gzippable-assets", "Defines the files to gzip")
+	val gzipAssets = TaskKey[Seq[File]]("gzip-assets", "gzip all assets")
+	lazy val gzipAssetsSetting = gzipAssets <<= gzipAssetsTask dependsOn (copyResources in Compile)
+	lazy val gzipAssetsTask = (gzippableAssets, streams) map {
+	  case (finder: PathFinder, s: TaskStreams) => {
+	    var count = 0
+	    var files = finder.get.map { file =>
+	      val gzTarget = new File(file.getAbsolutePath + ".gz")
+	      IO.gzip(file, gzTarget)
+	      count += 1;
+	      gzTarget
+	    }
+	    s.log.info("Compressed " + count + " asset(s)")
+	    files
+	  }
+	}
+	
+	val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+	    gzippableAssets <<= (classDirectory in Compile)(dir => (dir ** ("*.js" || "*.css" || "*.html"))),
+	    gzipAssetsSetting,
+	    playPackageEverything <<= playPackageEverything dependsOn gzipAssets
+	)
 }
+
+
